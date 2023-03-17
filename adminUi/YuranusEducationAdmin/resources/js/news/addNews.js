548da7c8383fc1556a4e5e7c31ea7e1af5f64c91
Kaushal Thakkar <kaushal.softvan@gmail.com>
2023-03-17 10:32:58 +0530

    intigret add news API.

--- adminUi/YuranusEducationAdmin/resources/js/news/addNews.js
+++ adminUi/YuranusEducationAdmin/resources/js/news/addNews.js
@@ -11,4 +11,38 @@ $(document).ready(function () {
             }
         },
     })
+
+    $("#contactSection").submit(function (e) {
+        if (!$("#contactSection").valid()) {
+            return false;
+        }
+
+        e.preventDefault();
+
+        let newsTitle = $("#newsTitle").val();
+        let newsDescription = $("#newsDescription").val();
+
+        let jsonRequest = {
+            "newsTitle": newsTitle,
+            "newsDescription": newsDescription,
+        };
+
+        /*JSON based Contact API Call with POST method*/
+        $.ajax({
+            type: "POST",
+            url: 'http://192.168.0.216:8088/newsInfo/insert',
+            cache: false,
+            crossDomain: true,
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(jsonRequest),
+            success: function () {
+                window.location.href = "viewNews.html";
+            },
+            error: function () {
+                console.log('Something Wrong.');
+            }
+        });
+
+    });
 });
